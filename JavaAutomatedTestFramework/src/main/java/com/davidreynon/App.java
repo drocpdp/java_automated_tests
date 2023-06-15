77791f27d681969bebe5c95e0c11acfd7b07d5e2
drocpdp <davidreynon@gmail.com>
2023-06-15 14:49:43 +0200

    baseline install

---
+++ JavaAutomatedTestFramework/src/main/java/com/davidreynon/App.java
@@ -0,0 +1,13 @@
+package com.davidreynon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
