b246ceb07e543d487e6b24bff586e79faf5a85fe
drocpdp <davidreynon@gmail.com>
2023-06-15 17:04:07 +0200

    removing tets from main/java/com

--- JavaAutomatedTestFramework/src/main/java/com/davidreynon/App.java
+++ JavaAutomatedTestFramework/src/main/java/com/davidreynon/App.java
@@ -1,8 +1,5 @@
 package com.davidreynon;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.*;
-
 /**
  * Hello world!
  *
@@ -12,9 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.setProperty("webdriver.chrome.driver", "/Users/davideynon/Projects/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.davidreynon.com");
-        driver.quit();
     }
 }
