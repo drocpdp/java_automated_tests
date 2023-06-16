642c5f8e80353bfd5eb977f63b61c0aeb2978a40
drocpdp <davidreynon@gmail.com>
2023-06-16 16:24:26 +0200

    upgrade to JUnit 5

--- JavaAutomatedTestFramework/src/test/java/com/davidreynon/AppTwoTest.java
+++ JavaAutomatedTestFramework/src/test/java/com/davidreynon/AppTwoTest.java
@@ -1,39 +1,20 @@
 package com.davidreynon;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
 public class AppTwoTest
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTwoTest( String testName )
-    {
-        super( testName );   
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+    @Test
     public void testApp()
     {
         System.out.println( "Hello World!" );
